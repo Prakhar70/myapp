592acd57a865071155ba05bf7f4b0f4a52ebadd9
Prakhar70 <42569845+Prakhar70@users.noreply.github.com>
2023-05-01 02:02:06 +0530

    sending data from view to templates

--- myapp/views.py
+++ myapp/views.py
@@ -1,9 +1,30 @@
+import datetime
 from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    
-    return render(request,"home.html",{})
+    date=datetime.datetime.now()
+    isActive=True
+    name="LearnCodeWithPrakhar"
+    list_of_programs=[
+        'WAP to check even or odd',
+        'WAP to check prime number',
+        'WAP to print all prime number',
+        'WAP to print pascals triangle'
+    ]
+    student={
+        'student_name':"Prakhar",
+        'student_college':"BIT Bangalore",
+        'student_city':"Lucknow"
+    }
+    data={
+        'date':date,
+        'isActive':isActive,
+        'name':name,
+        'list_of_programs':list_of_programs,
+        'student_data':student
+    }
+    return render(request,"home.html",data)
 
 def about(request):
 
