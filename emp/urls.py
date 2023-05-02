f5ca2242e0a5c16f5be5e2da454499da69cf8a23
Prakhar70 <42569845+Prakhar70@users.noreply.github.com>
2023-05-03 01:27:58 +0530

    employee mangement system completed

--- emp/urls.py
+++ emp/urls.py
@@ -20,4 +20,7 @@ from .views import *
 urlpatterns = [
     path("home/",emp_home),
     path("add_emp/",add_emp),
+    path("delete_emp/<int:emp_id>",delete_emp),
+    path("update_emp/<int:emp_id>",update_emp),
+    path("do_update_emp/<int:emp_id>",do_update_emp),
 ]
--- emp/views.py
+++ emp/views.py
@@ -33,3 +33,39 @@ def add_emp(request):
         print("data is coming")
         return redirect("/emp/home/")
     return render(request,"emp/add_emp.html",{})
+
+def delete_emp(request,emp_id):
+    emp=Emp.objects.get(pk=emp_id)
+    emp.delete()
+    return redirect("/emp/home/")
+
+def update_emp(request,emp_id):
+    emp=Emp.objects.get(pk=emp_id)
+    return render(request, "emp/update_emp.html",{
+        'emp':emp
+    })
+
+def do_update_emp(request,emp_id):
+    if request.method=='POST':
+        print(id)
+        emp_name=request.POST.get("emp_name")
+        emp_idd=request.POST.get("emp_id")
+        emp_phone=request.POST.get("emp_phone")
+        emp_address=request.POST.get("emp_address")
+        emp_working=request.POST.get("emp_working")
+        emp_department=request.POST.get("emp_department")
+
+        e=Emp.objects.get(pk=emp_id)
+        e.name=emp_name
+        e.emp_id=emp_idd
+        e.phone=emp_phone
+        e.address=emp_address
+        if emp_working is None:
+            e.working=False
+        else:
+            e.working=True
+        e.department=emp_department
+        e.save()
+
+
+    return redirect("/emp/home/")
